a842b7d95a52c1c4d5ccf090381775f78cd99b3f
yujiaxin22 <13680904552@163.com>
2019-06-12 17:45:34 +0800

    first commit

---
+++ src/main/java/com/juexun/etf/EtfApplication.java
@@ -0,0 +1,13 @@
+package com.juexun.etf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtfApplication.class, args);
+    }
+
+}
